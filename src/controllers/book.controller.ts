dee23010fca880d72a0e95545e9e437e57ee1089
Brad Preston <bap5393@gmail.com>
2023-08-20 18:17:59 -0400

    Added getOneBook to book controller

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -9,12 +9,39 @@ export function bookController(service: BookService) {
 			res.status(200);
 			res.json({ books });
 		} catch (err) {
-			res.status(404);
-			res.json({ error: err.message });
+			if (err instanceof ReferenceError) {
+				res.status(404);
+				res.json({ error: err.message });
+				return;
+			}
+			res.status(400);
+			res.json({ error: 'Something went wrong.' });
+		}
+	};
+
+	const getOneBook = async (req: Request, res: Response) => {
+		try {
+			const book: Book = await service.getOne(req.params.id);
+			res.status(200);
+			res.json({ book });
+		} catch (err) {
+			if (err instanceof ReferenceError) {
+				res.status(404);
+				res.json({ error: err.message });
+				return;
+			}
+			if (err instanceof TypeError) {
+				res.status(400);
+				res.json({ error: err.message });
+				return;
+			}
+			res.status(400);
+			res.json({ error: 'Something went wrong.' });
 		}
 	};
 
 	return {
-		getAllBooks: getAllBooks
+		getAllBooks: getAllBooks,
+		getOneBook: getOneBook
 	};
 }
