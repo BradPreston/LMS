7e244d51c1ecb60ee6e309995afe715cd8b1a8a0
bradpreston <bap5393@gmail.com>
2023-08-21 14:24:34 -0400

    Add simple starting unit tests with mocks

---
+++ __tests__/unit/config/singleton.ts
@@ -0,0 +1,15 @@
+import { mockDeep, mockReset, DeepMockProxy } from 'jest-mock-extended';
+
+import prisma from '../../../client';
+import { Client } from '../../../src/models/client';
+
+jest.mock('./client', () => ({
+	__esModule: true,
+	default: mockDeep<Client>()
+}));
+
+beforeEach(() => {
+	mockReset(prismaMock);
+});
+
+export const prismaMock = prisma as unknown as DeepMockProxy<Client>;
---
+++ __tests__/unit/service_tests.ts
@@ -0,0 +1,78 @@
+import { Book } from '@prisma/client';
+import { prismaMock } from './config/singleton';
+import { BookService } from '../../src/services/book.service';
+
+describe('Book Service', () => {
+	const service = new BookService(prismaMock);
+
+	it("shouldn't find any books", async () => {
+		const books: Book[] = [];
+		prismaMock.book.findMany.mockResolvedValue(books);
+
+		await expect(service.getAll()).rejects.toThrow(
+			new ReferenceError('No books were found.')
+		);
+	});
+
+	it('should have a length of 2 books', async () => {
+		const books: Book[] = [
+			{
+				id: 1,
+				title: 'Test Title',
+				author: 'Test Author',
+				publisher: 'Test Publisher',
+				isbn: '0123456789',
+				publication_year: 2023,
+				number_of_pages: 350,
+				available_copies: 2
+			},
+			{
+				id: 2,
+				title: 'Test Title',
+				author: 'Test Author',
+				publisher: 'Test Publisher',
+				isbn: '0123456789',
+				publication_year: 2023,
+				number_of_pages: 350,
+				available_copies: 2
+			}
+		];
+
+		prismaMock.book.findMany.mockResolvedValue(books);
+
+		await expect(service.getAll()).resolves.toHaveLength(2);
+	});
+
+	it('should find one book', async () => {
+		const book = {
+			id: 1,
+			title: 'Test Title',
+			author: 'Test Author',
+			publisher: 'Test Publisher',
+			isbn: '0123456789',
+			publication_year: 2023,
+			number_of_pages: 350,
+			available_copies: 2
+		};
+		prismaMock.book.findFirstOrThrow.mockResolvedValue(book);
+
+		await expect(service.getOne('1')).resolves.toEqual({
+			id: 1,
+			title: 'Test Title',
+			author: 'Test Author',
+			publisher: 'Test Publisher',
+			isbn: '0123456789',
+			publication_year: 2023,
+			number_of_pages: 350,
+			available_copies: 2
+		});
+	});
+
+	it('should error if no book is found', async () => {
+		prismaMock.book.findFirstOrThrow.mockImplementation();
+
+		await expect(service.getOne('1')).rejects.toEqual(
+			new ReferenceError('No book with id "1" was found.')
+		);
+	});
+});
