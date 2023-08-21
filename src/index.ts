dcefde4acad0fdb543789a83a85f085dadedc428
bradpreston <bap5393@gmail.com>
2023-08-21 14:23:49 -0400

    Update index file to get env based on npm script

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,30 @@
 import express from 'express';
 import { Client } from './models/client';
 import { router } from './routes/router';
+import dotenv from 'dotenv';
+import { exit } from 'process';
+
+const env = dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+if (env.error) {
+	console.error(env.error.message);
+	exit(1);
+}
+
+const PORT = process.env.PORT || 3000;
+
 const app = express();
-const client = new Client();
+const client = new Client({
+	datasources: {
+		db: {
+			url: process.env.DATABASE_URL
+		}
+	}
+});
 
 app.use(express.json());
 
 router(app, client);
 
-app.listen(3000, () => {
-	console.log('Listening on port 3000');
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`);
 });
